e8c317a828ee5055cd5b7b4cf09a69f704ae76ff
Дима Ш. <dima.o.sh@proton.me>
2025-07-21 15:45:01 +0100

    Install LibreWolf and uninstall Firefox
    * LibreWolf has better default settings than Firefox for privacy, so I will try using it as my main browser.

--- configuration.nix
+++ configuration.nix
@@ -92,15 +92,14 @@
     ];
   };
 
-  # Install firefox.
-  programs.firefox.enable = true;
-
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget,
     git
+
+    librewolf
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
