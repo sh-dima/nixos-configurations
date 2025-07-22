efea6e08f8ea7f66699bdfa1de403ae5c6eb91ed
Дима Ш. <dima.o.sh@proton.me>
2025-07-22 16:54:22 +0100

    Update flake.nix file
    * Updated the flake.nix file to be a valid Nix flake for my NixOS configuration.

--- flake.nix
+++ flake.nix
@@ -6,10 +6,11 @@
   };
 
   outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
   };
 }
