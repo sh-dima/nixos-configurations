73516f926e08edb646bdd8e50975e43ff8246508
Дима Ш. <dima.o.sh@proton.me>
2025-07-30 13:44:58 +0100

    Move my home configuration to a separate file
    * This helps organise the system.

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -122,21 +122,7 @@
   services.flatpak.enable = true;
 
   home-manager.backupFileExtension = "bak";
-  home-manager.users.enderman = {
-    home.stateVersion = "24.11";
-
-    programs = {
-      git = (import ../../modules/generic/git.nix)
-        // (import ../../modules/personal/git.nix)
-        // { enable = true; };
-
-      vscode = (import ../../modules/generic/vscode.nix { inherit pkgs; })
-        // { enable = true; };
-
-      librewolf = (import ../../modules/generic/librewolf.nix {inherit pkgs inputs; })
-        // { enable = true; };
-    };
-  };
+  home-manager.users.enderman = (import ./users/enderman.nix { inherit inputs pkgs; });
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
