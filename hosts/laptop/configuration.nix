03288355bb13d0df073a1aee4303415177f8c079
Дима Ш. <dima.o.sh@proton.me>
2025-08-03 17:55:07 +0100

    Allow unfree packages

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -94,6 +94,8 @@
     extraGroups = [ "networkmanager" "wheel" ];
   };
 
+  nixpkgs.config.allowUnfree = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
