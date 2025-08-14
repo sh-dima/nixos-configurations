0dbb5c3c35e0ba1a0cb019c005fb4bfa0a66f0a4
Дима Ш. <dima.o.sh@proton.me>
2025-08-14 19:55:52 +0100

    Configure Home Manager for my PC

--- hosts/pc/configuration.nix
+++ hosts/pc/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
   imports =
@@ -103,6 +103,13 @@
     git
     vscodium
   ];
+
+  home-manager.sharedModules = [
+    inputs.plasma-manager.homeManagerModules.plasma-manager
+  ];
+
+  home-manager.backupFileExtension = "bak";
+  home-manager.users.enderman = (import ./users/enderman.nix { inherit inputs pkgs; });
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
