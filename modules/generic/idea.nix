610b708bae84b60f050ea85a0362c53f55c4700a
Дима Ш. <dima.o.sh@proton.me>
2025-09-01 17:04:34 +0100

    Configure IntelliJ IDEA CE: install Minecraft Development plugin

--- modules/generic/idea.nix
+++ modules/generic/idea.nix
@@ -4,6 +4,7 @@
   environment.systemPackages = [
     (pkgs.jetbrains-plugins.lib.buildIdeWithPlugins pkgs.jetbrains.idea-community-src (with pkgs.jetbrains-plugins; [
       com.github.deeepamin.gitlabciaid."1.0.5"
+      com.demonwav.minecraft-dev."2024.3-1.8.2"
     ]))
   ];
 }
