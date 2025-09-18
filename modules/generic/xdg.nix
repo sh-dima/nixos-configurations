89e426e321ce292caca7b21f97021eae268b74bd
Дима Ш. <dima.o.sh@proton.me>
2025-09-18 18:39:04 +0100

    Configure XDG: open HTML files with LibreWolf

--- modules/generic/xdg.nix
+++ modules/generic/xdg.nix
@@ -2,6 +2,7 @@
   defaultApplications = {
     "application/xml" = "codium.desktop";
     "application/java-archive" = "jadx.desktop";
+    "application/xhtml+xml" = "librewolf.desktop";
 
     "text/csv" = "codium.desktop";
   };
