7a2ef2caaf7d34515168cc275c1ea5f37fe40d06
Дима Ш. <dima.o.sh@proton.me>
2025-09-21 10:42:56 +0100

    Implement real Git info

--- modules/personal/git.nix
+++ modules/personal/git.nix
@@ -1,6 +1,15 @@
+{ config, ... }:
+
 {
   programs.git = {
     userName = "Esoteric Enderman";
     userEmail = "EsotericEnderman@proton.me";
+
+    extraConfig = {
+      alias = {
+        real = "! git config user.name \"$(cat ${config.sops.secrets."git/username".path})\" && git config user.email $(cat ${config.sops.secrets."git/email".path}) && git profile";
+        fake = "! git config user.name \"Esoteric Enderman\" && git config user.email EsotericEnderman@proton.me && git profile";
+      };
+    };
   };
 }
