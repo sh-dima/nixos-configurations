2394595cfabbf416e4183c47379ecd3eb83ad699
Дима Ш. <dima.o.sh@proton.me>
2025-09-29 13:15:35 +0100

    Decide on a final Git profile

--- modules/personal/git.nix
+++ modules/personal/git.nix
@@ -1,15 +1,8 @@
-{ config, ... }:
+{ ... }:
 
 {
   programs.git = {
-    userName = "Dima FYI";
+    userName = "Дима Ш.";
     userEmail = "DimaFYI@proton.me";
-
-    extraConfig = {
-      alias = {
-        real = "! git config user.name \"$(cat ${config.sops.secrets."git/username".path})\" && git config user.email $(cat ${config.sops.secrets."git/email".path}) && git profile";
-        fake = "! git config user.name \"Dima FYI\" && git config user.email DimaFYI@proton.me && git profile";
-      };
-    };
   };
 }
