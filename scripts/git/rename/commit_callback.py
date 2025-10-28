f10fbc8a2430912902b947236ceff26fa53e4a3c
Дима Ш. <dima.o.sh@proton.me>
2025-10-28 12:46:08 +0000

    Configure Git: make rename script update trailers
    Made the Git rename authors script support renaming trailers such as "Co-authored-by".

--- scripts/git/rename/commit_callback.py
+++ scripts/git/rename/commit_callback.py
@@ -1,3 +1,8 @@
+import re
+
+from re import Match
+from typing import AnyStr
+
 with open('.git/filter-repo/rename/old-name', 'r') as file:
 	old_name = file.read().strip()
 
@@ -16,3 +21,24 @@ if commit.author_name.decode('utf-8') == old_name and commit.author_email.decode
 if commit.committer_name.decode('utf-8') == old_name and commit.committer_email.decode('utf-8') == old_email:
 		commit.committer_name = new_name.encode('utf-8')
 		commit.committer_email = new_email.encode('utf-8')
+
+trailer_pattern = re.compile(
+	r'(?m)^(?P<tag>[A-Za-z-]+): '
+	r'(?P<name>[^\n]+)(?= <[^\n]*>$)'
+	r' '
+	r'<(?P<email>[^\n]*)>$'
+)
+
+def replacer(match: Match[AnyStr]) -> AnyStr:
+	tag = match.group('tag')
+	name = match.group('name')
+	email = match.group('email')
+
+	if name == old_name and email == old_email:
+			return f"{tag}: {new_name} <{new_email}>"
+	else:
+			return match.group(0)
+
+message: str = commit.message.decode('utf-8')
+
+commit.message = trailer_pattern.sub(replacer, message).encode('utf-8')
