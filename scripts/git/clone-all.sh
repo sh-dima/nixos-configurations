8a41d7c5eed0e208e2ac5b56e6cd2a62fc8cd35c
Дима Ш. <dima.o.sh@proton.me>
2025-11-13 09:40:00 +0000

    Configure Git: fix typo in script
    This script clones GitHub repositories, not Gists. (Gist script coming soon)

--- scripts/git/clone-all.sh
+++ scripts/git/clone-all.sh
@@ -8,7 +8,7 @@ if [ -z "$limit" ]; then
 	limit=30
 fi
 
-echo "Cloning all Gists in namespace $namespace with repository limit of $limit"
+echo "Cloning all repositories in namespace $namespace with repository limit of $limit"
 
 gh repo list "$namespace" --limit "$limit" | while read -r repository _; do
 	gh repo clone "$repository" "$repository"
