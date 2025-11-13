d9dda99eaa81c35ba0c415d65b2711f1ae79f60a
Дима Ш. <dima.o.sh@proton.me>
2025-11-13 17:09:06 +0000

    Add a Python development shell

--- flake.nix
+++ flake.nix
@@ -9,6 +9,8 @@
 
     sops-nix.url = "github:mic92/sops-nix";
 
+    flake-utils.url = "github:numtide/flake-utils";
+
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
     home-manager.url = "github:nix-community/home-manager?ref=release-25.05";
     plasma-manager.url = "github:nix-community/plasma-manager";
@@ -22,47 +24,70 @@
     prismlauncher.url = "github:PrismLauncher/PrismLauncher";
   };
 
-  outputs = { nixpkgs, home-manager, nix-flatpak, nix4vscode, prismlauncher, ... }@inputs: {
-    nixosConfigurations.pc = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./hosts/pc/configuration.nix
-        ./hosts/pc/hardware-configuration.nix
-        home-manager.nixosModules.default
+  outputs = { nixpkgs, home-manager, nix-flatpak, nix4vscode, prismlauncher, flake-utils, ... }@inputs:
+    let
+      nixosConfigurations = {
+        pc = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./hosts/pc/configuration.nix
+            ./hosts/pc/hardware-configuration.nix
+            home-manager.nixosModules.default
 
-        nix-flatpak.nixosModules.nix-flatpak
+            nix-flatpak.nixosModules.nix-flatpak
 
-        {
-          nixpkgs.overlays = [
-            nix4vscode.overlays.forVscode
-            inputs.nix-jetbrains-plugins.overlays.default
-            prismlauncher.overlays.default
+            {
+              nixpkgs.overlays = [
+                nix4vscode.overlays.forVscode
+                inputs.nix-jetbrains-plugins.overlays.default
+                prismlauncher.overlays.default
+              ];
+            }
           ];
-        }
-      ];
 
-      specialArgs = { inherit inputs; };
-    };
+          specialArgs = { inherit inputs; };
+        };
 
-    nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./hosts/laptop/configuration.nix
-        ./hosts/laptop/hardware-configuration.nix
-        home-manager.nixosModules.default
+        laptop = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./hosts/laptop/configuration.nix
+            ./hosts/laptop/hardware-configuration.nix
+            home-manager.nixosModules.default
 
-        nix-flatpak.nixosModules.nix-flatpak
+            nix-flatpak.nixosModules.nix-flatpak
 
-        {
-          nixpkgs.overlays = [
-            nix4vscode.overlays.forVscode
-            inputs.nix-jetbrains-plugins.overlays.default
-            prismlauncher.overlays.default
+            {
+              nixpkgs.overlays = [
+                nix4vscode.overlays.forVscode
+                inputs.nix-jetbrains-plugins.overlays.default
+                prismlauncher.overlays.default
+              ];
+            }
           ];
-        }
-      ];
 
-      specialArgs = { inherit inputs; };
-    };
-  };
+          specialArgs = { inherit inputs; };
+        };
+      };
+    in
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.python = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python3
+            python3Packages.pip
+            python3Packages.virtualenv
+          ];
+
+          shellHook = ''
+            if [ ! -d .venv ]; then
+              python -m venv .venv
+            fi
+
+            source .venv/bin/activate
+          '';
+        };
+      }) // { inherit nixosConfigurations; };
 }
