298b5aff7d577ef0cfd938729c7f8a10bfa6c6f3
Дима Ш. <dima.o.sh@proton.me>
2025-12-04 20:50:01 +0000

    Configure Git: add script to sync KDE Connect
    Added a script that forces the file systems of connected KDE Connect devices to be loaded.

---
+++ scripts/git/sync.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import sys
+import subprocess
+import re
+
+remote = subprocess.run(
+	["git", "rev-parse", "--abbrev-ref", "--symbolic-full-name", "@{u}"],
+	capture_output=True,
+	text=True,
+).stdout.split("/")[0]
+
+url = subprocess.run(
+	["git", "remote", "get-url", remote],
+	capture_output=True,
+	text=True,
+).stdout
+
+connect = re.sub("/run/user/\\d+/", "", url).split("/")[0]
+
+try:
+	parsed = int(connect, 16)
+except:
+	print("Current remote is not a KDE Connect remote!")
+	exit(1)
+
+subprocess.run(
+	["kioclient", "ls", f"kdeconnect://" + connect],
+	capture_output=True,
+)
+
+subprocess.run(
+	["git", "pull"],
+)
