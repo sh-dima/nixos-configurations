4ce252a10770a078d4c11c294aec88ba4cc385f3
Дима Ш. <dima.o.sh@proton.me>
2025-12-07 23:44:05 +0000

    Update flake
    Also updated nixpkgs channel from 25.05 to 25.11.

--- flake.nix
+++ flake.nix
@@ -11,8 +11,8 @@
 
     flake-utils.url = "github:numtide/flake-utils";
 
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
-    home-manager.url = "github:nix-community/home-manager?ref=release-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    home-manager.url = "github:nix-community/home-manager?ref=release-25.11";
     plasma-manager.url = "github:nix-community/plasma-manager";
 
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
