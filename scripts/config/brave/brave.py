076eab01e2f8412498cb777074e121802a4a1349
Дима Ш. <dima.o.sh@proton.me>
2025-12-28 22:12:12 +0000

    Configure Brave

---
+++ scripts/config/brave/brave.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import json
+
+path = "/home/dima/.config/BraveSoftware/Brave-Browser/Default/Preferences"
+backup = "/home/dima/.config/BraveSoftware/Brave-Browser/Default/Preferences.bak"
+
+with open(path) as file:
+	configuration = file.read()
+
+with open(backup, "w") as file:
+	file.write(configuration)
+
+dictionary = json.loads(configuration)
+
+dictionary["brave"]["ai_chat"]["show_toolbar_button"] = False
+
+dictionary["brave"]["branded_wallpaper_notification_dismissed"] = True
+dictionary["brave"]["brave_ads"]["should_allow_ads_subdivision_targeting"] = False
+
+dictionary["brave"]["enable_window_closing_confirm"] = False
+dictionary["brave"]["has_seen_brave_welcome_page"] = True
+
+dictionary["brave"]["new_tab_page"]["show_brave_news"] = False
+dictionary["brave"]["new_tab_page"]["show_branded_background_image"] = False
+dictionary["brave"]["new_tab_page"]["show_rewards"] = False
+dictionary["brave"]["new_tab_page"]["show_together"] = False
+
+dictionary["brave"]["new_tab_page"]["background"]["random"] = False
+dictionary["brave"]["new_tab_page"]["background"]["type"] = "color"
+dictionary["brave"]["new_tab_page"]["background"]["selected_value"] = "#000000"
+
+dictionary["brave"]["rewards"]["show_brave_rewards_button_in_location_bar"] = False
+
+serialized = json.dumps(
+	dictionary,
+	indent="\t",
+	ensure_ascii=False,
+	sort_keys=True,
+)
+
+with open(path, "w", encoding='utf-8') as file:
+	file.write(serialized)
