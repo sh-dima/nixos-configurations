220a61de96a1c15b166cfc09fb9ce0da4fe9d338
Дима Ш. <dima.o.sh@proton.me>
2026-01-09 09:32:24 +0000

    Configure Git: ignore partially downloaded files

--- modules/generic/git.nix
+++ modules/generic/git.nix
@@ -112,6 +112,7 @@
       ".bash_history"
       ".bash_history-[0-9][0-9][0-9][0-9][0-9].tmp"
       ".python_history"
+      "*.crdownload"
       ".Trash-*"
       "id_ed25519"
       "known_hosts"
