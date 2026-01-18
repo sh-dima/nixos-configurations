190190895b319098c8556d0cec7304112421cb91
Дима Ш. <dima.o.sh@proton.me>
2026-01-18 21:08:37 +0000

    Configure XDG: open HTML files with Brave

--- modules/generic/xdg.nix
+++ modules/generic/xdg.nix
@@ -2,7 +2,9 @@
   defaultApplications = {
     "application/xml" = "codium.desktop";
     "application/java-archive" = "jadx.desktop";
+    "application/xhtml+xml" = "brave.desktop";
 
+    "text/html" = "brave.desktop";
     "text/csv" = "codium.desktop";
 
     "image/x-adobe-dng" = "org.kde.gwenview.desktop";
