c38dac6eef16401e17f1d9a1d47dd355094406f7
Дима Ш. <dima.o.sh@proton.me>
2026-01-27 10:18:48 +0000

    Configure Git: use trash to find deleted time

--- scripts/git/update.py
+++ scripts/git/update.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 import os
+import sys
 import subprocess
+
+from pathlib import Path
+from urllib.parse import quote
 
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from datetime import datetime
@@ -33,13 +37,32 @@ else:
 			# If the deletion of a file is staged, it is impossible to know when that file was deleted.
 			# For that reason, it is impossible to know what date and time should be attributed to the commit.
 			# Therefore, the normal git commit functionality is used.
-			exit(subprocess.run(["git", "commit"]).returncode)
 
-		changed_time = subprocess.run(["stat", file], capture_output=True).stdout.decode("utf-8").splitlines()[6].removeprefix("Change: ")
-		fixed = changed_time[:19] + changed_time[29:]
-		date = datetime.strptime(fixed, "%Y-%m-%d %H:%M:%S %z")
+			# Unless we look in the trash to figure it out:
+			trash = f"{Path.home()}/.local/share/Trash/"
+			info = os.listdir(f"{trash}/info")
+
+			date = None
+			for trash_info in info:
+				with open(f"{trash}/info/{trash_info}") as info_file:
+					lines = info_file.readlines()
+
+				path = lines[1].removeprefix("Path=").strip()
+				target = quote(f"{os.getcwd()}/{file}").strip()
+
+				if target == path:
+					time = lines[2].removeprefix("DeletionDate=").strip()
+					date = datetime.strptime(time, "%Y-%m-%dT%H:%M:%S") # Example: 2025-11-25T08:48:09
+
+			if not date:
+				logging.debug("Couldn't find date for deleted file. Using default commit behavior...")
+				exit(subprocess.run(["git", "commit"]).returncode)
+		else:
+			changed_time = subprocess.run(["stat", file], capture_output=True).stdout.decode("utf-8").splitlines()[6].removeprefix("Change: ")
+			fixed = changed_time[:19] + changed_time[29:]
+			date = datetime.strptime(fixed, "%Y-%m-%d %H:%M:%S %z")
+
 		timestamp = date.timestamp()
-
 		logging.debug(f"{file}: {date}")
 
 		if timestamp > latest:
