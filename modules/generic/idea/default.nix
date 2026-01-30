70192c0d287815d0210f77551e7d58c35e5d13b3
Дима Ш. <dima.o.sh@proton.me>
2026-01-30 09:56:39 +0000

    Configure IntelliJ IDEA: use tabs and smart tabs

--- modules/generic/idea/default.nix
+++ modules/generic/idea/default.nix
@@ -9,8 +9,11 @@
 
     data = ''
       mkdir -p /home/dima/.config/JetBrains/IdeaIC2025.1/options/
+      mkdir -p /home/dima/.config/JetBrains/IdeaIC2025.1/codestyles/
+
       cat ${./project.default.xml} > /home/dima/.config/JetBrains/IdeaIC2025.1/options/project.default.xml
       cat ${./filetypes.xml} > /home/dima/.config/JetBrains/IdeaIC2025.1/options/filetypes.xml
+      cat ${./codestyles/Default.xml} > /home/dima/.config/JetBrains/IdeaIC2025.1/codestyles/Default.xml
     '';
   };
 }
